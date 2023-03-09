f0f9435dd932b6911862655f036e4cae5ac195cf
alammehtab <mehtaba728@gmail.com>
2023-03-09 11:52:49 +0500

    minor fix

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -15,7 +15,10 @@ const register = async (userData) => {
 
 // login user
 const login = async (userData) => {
-  const response = await axios.post(API_URL + "login", userData);
+  const response = await axios.post(
+    "https://my-diary-backend-netlify-production.up.railway.app/api/v1/users/login",
+    userData
+  );
 
   if (response.data) {
     localStorage.setItem("user", JSON.stringify(response.data));
